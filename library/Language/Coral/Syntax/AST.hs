2205aa83ef7c0850eb736a8c0b64f9d02041932e
Luis Holanda <luiscmholanda@gmail.com>
2018-10-11 20:22:40 -0300

    Update .gitignore

--- library/Language/Coral/Syntax/AST.hs
+++ library/Language/Coral/Syntax/AST.hs
@@ -1,7 +1,7 @@
 module Language.Coral.Syntax.AST where
 
-import Data.Data
-import Data.Text as T
+import           Data.Data
+import           Data.Text as T
 
 data Ident = Ident
   { name :: !T.Text
@@ -9,7 +9,7 @@ data Ident = Ident
 
 data Module =
   Module { private :: Suite -- private declarations
-         , public :: Suite -- exposed declarations
+         , public  :: Suite -- exposed declarations
          }
   deriving (Eq, Ord, Show, Typeable, Data)
 
@@ -19,7 +19,7 @@ type DottedName = [Ident]
 
 data ImportItem = ImportItem
   { itemName :: DottedName
-  , asName :: Maybe Ident
+  , asName   :: Maybe Ident
   } deriving (Eq, Ord, Show, Typeable, Data)
 
 data FromItems
@@ -27,8 +27,24 @@ data FromItems
   | OnlyItems [Ident]
   deriving (Eq, Ord, Show, Typeable, Data)
 
-data TypeDef = Sum | Product | Record
+data TypeDef
+  = ADT { typeName     :: Type
+        , constructors :: [Constructor]}
+  | Record { typeName :: Type
+           , fields   :: [Field]}
   deriving (Eq, Ord, Show, Typeable, Data)
+
+data Constructor
+   = ConstRecord { constName   :: Ident
+                 , constFields :: [Field]}
+   | Product { constName :: Ident
+             , constTyped :: [Type]}
+   | Term { constName :: Ident }
+   deriving (Eq, Ord, Show, Typeable, Data)
+
+data Field = Field { fieldName :: Ident
+                   , fieldType :: Type }
+    deriving (Eq, Ord, Show, Typeable, Data)
 
 data DataDef = DataDef
   deriving (Eq, Ord, Show, Typeable, Data)
@@ -37,43 +53,43 @@ data Statement
   = Import { importedItems :: [ImportItem] }
   | TypeD TypeDef
   | DataD DataDef
-  | While { cond :: Expr
-          , body :: Suite
-          , else' :: Suite }
-  | For { targets :: [Expr]
+  | While { cond  :: Expr
+          , body  :: Suite
+          , else' :: Maybe Suite }
+  | For { targets   :: [Expr]
         , generator :: Expr
-        , body :: Suite
-        , else' :: Suite }
+        , body      :: Suite
+        , else'     :: Maybe Suite }
   | AsyncFor Statement
   | Fun { funcName :: Ident
-        , params :: [Parameter]
-        , retType :: Type
-        , body :: Suite }
+        , params   :: [Parameter]
+        , retType  :: Type
+        , body     :: Suite }
   | AsyncFun Statement
   | Conditional { guards :: [(Expr, Suite)]
-                , else' :: Suite }
-  | Assign { to :: Expr
+                , else'  :: Maybe Suite }
+  | Assign { to   :: Expr
            , expr :: Expr }
-  | MutAssign { to :: Expr
+  | MutAssign { to   :: Expr
               , expr :: Expr }
   | Decorated { decorators :: [Decorator]
               , decoraterd :: Statement }
   | Return { retExpr :: Maybe Expr }
-  | Try { body :: Suite
-        , excepts :: [Handler]
-        , handlers :: [DottedName]
-        , else' :: Suite
-        , finally :: Suite }
+  | Try { body     :: Suite
+        , excepts  :: [Handler]
+        , handlers :: [Expr]
+        , else'    :: Maybe Suite
+        , finally  :: Maybe Suite }
   | Raise { expr :: Expr }
   | With { context :: [(Expr, Maybe Expr)]
-         , body :: Suite }
+         , body    :: Suite }
   | AsyncWith Statement
   | Pass
   | Break
   | Continue
   | Delete [Expr]
-  | VarDef { varName :: Ident
-           , varType :: Type
+  | VarDef { varName  :: Ident
+           , varType  :: Type
            , varValue :: Maybe Expr }
   | Expression Expr
   | Assert Expr
@@ -81,12 +97,12 @@ data Statement
 
 data Decorator = Decorator
   { decName :: DottedName
-  , args :: [Argument]
+  , args    :: [Argument]
   } deriving (Eq, Ord, Show, Typeable, Data)
 
 data Parameter
-  = Param { paramName :: Ident
-          , paramType :: Type
+  = Param { paramName    :: Ident
+          , paramType    :: Type
           , paramDefault :: Maybe Expr }
   | VarParam { paramName :: Ident
              , paramType :: Type }
@@ -101,7 +117,7 @@ data Argument
   deriving (Eq, Ord, Show, Typeable, Data)
 
 data Handler = Handler
-  { clause :: ExceptClause
+  { clause  :: ExceptClause
   , hanBody :: Suite
   } deriving (Eq, Ord, Show, Typeable, Data)
 
@@ -111,7 +127,7 @@ data ExceptClause =
 
 data Comprehension = Comprehension
   { compExpr :: CompExpr
-  , compFor :: CompFor
+  , compFor  :: CompFor
   } deriving (Eq, Ord, Show, Typeable, Data)
 
 data CompExpr
@@ -120,14 +136,14 @@ data CompExpr
   deriving (Eq, Ord, Show, Typeable, Data)
 
 data CompFor = CompFor
-  { async :: Bool
-  , exprs :: [Expr]
+  { async  :: Bool
+  , exprs  :: [Expr]
   , inExpr :: Expr
-  , iters :: Maybe CompIter
+  , iters  :: Maybe CompIter
   } deriving (Eq, Ord, Show, Typeable, Data)
 
 data CompIf = CompIf
-  { ifExpr :: Expr
+  { ifExpr  :: Expr
   , ifIters :: Maybe CompIter
   } deriving (Eq, Ord, Show, Typeable, Data)
 
@@ -136,21 +152,32 @@ data CompIter
   | IterIf CompIf
   deriving (Eq, Ord, Show, Typeable, Data)
 
-data Expr
-  = Var Ident
-  | Int Integer
+data Str
+  = Str !T.Text
+  | RawStr Str
+  | FormatStr Str
+  | ByteStr Str
+  | SplitStr Str Str
+  deriving (Eq, Ord, Show, Typeable, Data)
+
+data Number
+  = Int Integer
   | Float Double
   | Imaginary Double
+  deriving (Eq, Ord, Show, Typeable, Data)
+
+data Expr
+  = Var Ident
+  | Num Number
   | Bool Bool
   | None
-  | ByteString !T.Text
-  | String !T.Text
-  | Call { func :: Expr
+  | String Str
+  | Call { func     :: Expr
          , callArgs :: [Argument] }
   | Indexing Expr -- Indexed
              Expr -- Index
-  | Cond { trueBranch :: Expr
-         , condition :: Expr
+  | Cond { trueBranch  :: Expr
+         , condition   :: Expr
          , falseBranch :: Expr }
   | BinaryOp Op
              Expr -- Left
@@ -164,6 +191,7 @@ data Expr
            Expr
   | Tuple [Expr]
   | Yield (Maybe YieldArg)
+  | Slice (Maybe Expr) (Maybe Expr) (Maybe Expr)
   | Generator Comprehension
   | Await Expr
   | ListComp Comprehension
